d3ad08b6019c77d293d05cb69d1aff2351ca20ac
nvm <24310@student.vitkovickastredni.cz>
2024-10-20 18:04:41 +0200

    Update ukol2.py

--- ukol2.py
+++ ukol2.py
@@ -1,2 +1,2 @@
-odd_numbers = [num for num in range(1, 21) if num % 2 != 0] #num nastaví se na aktuální číslo vrací zbytek po dělení. Pokud num % 2 vrátí 0, znamená to, že je číslo sudé (dělí se 2 bez zbytku). Pokud vrátí něco jiného (v tomto případě 1), číslo je liché.
+odd_numbers = [num for num in range(1, 21) if num % 2 != 0] #num nastaví se na aktuální číslo.Když num % 2 vrátí 0, znamená to, že je číslo sudé (dělí se 2 bez zbytku). Pokud vrátí něco jiného (v tomto případě 1), číslo je liché.
 print(odd_numbers)
