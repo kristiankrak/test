11435d7806f43383d7071722517fbb5b4cc884f9
nvm <24310@student.vitkovickastredni.cz>
2024-10-20 15:09:18 +0200

    úkol na seřazení čísel

---
+++ ukol.py
@@ -0,0 +1,8 @@
+a = 15 #zasazení hodnoty
+b = 8
+c = 23
+
+maximum = max(a, b, c) #najde maximum
+minimum = min(a, b, c) #najde minimum
+middle = a + b + c - maximum -  minimum #najde střed odečte minimum a maximum od všech čísel :D
+print(maximum, middle, minimum) #print
